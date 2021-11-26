0cd79b80f8646a4ac31ea9b8c2d3a5621466bc04
Altryd <michael_bor@mail.ru>
2021-11-26 20:17:11 +0400

    Added docstrings to functions

--- package_validate_and_sort/Sort.py
+++ package_validate_and_sort/Sort.py
@@ -4,15 +4,24 @@ import package_validate_and_sort.Validator_module
 from tqdm import tqdm
 
 
-def key_value(some_list: list):
+def key_value(some_list: list) -> any:
     return some_list[1]
 
 
-def height_value(some_dict: dict):
+def height_value(some_dict: dict) -> any:
     return some_dict['height']
 
 
 def insertion_sort(list_to_sort: list, key_function) -> list:
+    """
+    Сортирует list_to_sort (список словарей) с применением алгоритма "Сортировка вставками", алгоритм. сложность: O(n^2)
+    :param list_to_sort: list
+        список, который требуется отсортировать
+    :param key_function: function
+        функция, указывающая, по какому полю списка сортировать данные
+    :return:
+        Отсортированный по возрастанию список в соответствии с указанной key_function
+    """
     with tqdm(list_to_sort, desc='Сортируем считанные данные') as progressbar:
         for i in range(1, len(list_to_sort)):
             value = list_to_sort[i]
@@ -27,17 +36,38 @@ def insertion_sort(list_to_sort: list, key_function) -> list:
 
 
 def read_valid_txt(file: str) -> list:
+    """
+    Считывает файл с валидными записями по пути file
+    :param file: str
+        Путь до файла
+    :return:
+        Список считанных записей
+    """
     with open(file, mode='rb') as read_from:
         _data = pickle.load(read_from)
         return _data
 
 
 def serialize_data_to_pickle(data_to_serialize: list, path: str) -> None:
+    """
+    Сериализует данные по указанному в path пути с помощью pickle
+    :param data_to_serialize: list
+        Список данных/записей
+    :param path: str
+        Путь к файлу, в который необходимо сериализовать данные
+    """
     with open(path, mode='wb') as write_to:
         pickle.dump(data_to_serialize, write_to)
 
 
 def serialize_validators_to_pickle(data_list: list, file: str) -> None:
+    """
+    Сериализует объекты типа Validator по пути path с помощью pickle
+    :param data_list: list
+        Список объектов файла Validator
+    :param file: str
+        Путь к файлу, в который необходимо сериализовать данные
+    """
     data_to_serialize = []
     with tqdm(data_list, desc='Сериализуем данные в файл') as progressbar:
         for i in range(0, len(data_list)):
@@ -58,6 +88,13 @@ def serialize_validators_to_pickle(data_list: list, file: str) -> None:
 
 
 def serialize_validators_to_json(data_list: list, file: str) -> None:
+    """
+    Сериализует объекты типа Validator по пути path с помощью json
+    :param data_list: list
+        Список объектов файла Validator
+    :param file: str
+        Путь к файлу, в который необходимо сериализовать данные
+    """
     data_to_serialize = []
     with tqdm(data_list, desc='Сериализуем данные в файл JSON, преобразовываем классы в словари') as progressbar:
         for i in range(0, len(data_list)):
@@ -68,6 +105,13 @@ def serialize_validators_to_json(data_list: list, file: str) -> None:
 
 
 def deserialize_validators_from_pickle(file: str) -> list:
+    """
+    Десериализует объекты типа Validator из файла по пути file
+    :param file: str
+        Путь к файлу, из которого необходимо десериализовать объекты типа Validator
+    :return:
+        Список десериализованных объектов типа Validator
+    """
     with open(file, mode='rb') as read_from:
         data_deserialized = pickle.load(read_from)
         return data_deserialized
