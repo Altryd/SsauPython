588eb61278182d07d85d48ca1967141729f7ac82
Altryd <michael_bor@mail.ru>
2021-11-30 16:04:19 +0400

    Fourth lab done

--- main.py
+++ main.py
@@ -1,123 +1,93 @@
-from package_validate_and_sort.Validator_module import Validator
-from package_validate_and_sort.Validator_module import File
-from package_validate_and_sort.Sort import *
 import argparse
-import os
-import json
+import time
+from multiprocessing import Pool
 
 
-parser = argparse.ArgumentParser(description='Sort.py')
+parser = argparse.ArgumentParser(description='dispersion.py')
 parser.add_argument(
-    '-i',
-    '-input',
-    type=str,
-    help='Аргумент, указывающий путь к файлу, который требуется отсортировать/проверить на валидность',
-    required=True,
-    dest='file_input')
-parser.add_argument(
-    '-o',
-    '-output',
-    type=str,
-    help='Аргумент, указывающий путь к файлу, в который требуется записать обработанные данные',
-    required=True,
-    dest='file_output')
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument(
-    '-v'
-    '-validate',
-    type=str,
-    help='Аргумент, показывающий, что необходимо проверить данные на корректность',
-    dest='validate'
-)
-group.add_argument(
-    '-s'
-    '-sort',
-    type=str,
-    help='Аргумент, показывающий, что необходимо отсортировать данные',
-    dest='sort'
-)
-parser.add_argument(
-    '-c',
-    '-count',
+    '-f',
+    '-first_number',
     type=int,
-    help='Аргумент, показывающий, сколько записей, начиная с начала нужно отсортировать и поместить в новый файл',
-    dest='count'
+    help='Первое число',
+    required=True,
+    dest='first_number')
+parser.add_argument(
+    '-s',
+    '-second_number',
+    type=int,
+    help='Второе число',
+    required=True,
+    dest='second_number')
+parser.add_argument(
+    '-t',
+    '-third_number',
+    type=int,
+    help='Третье число',
+    required=True,
+    dest='third_number'
 )
-args = parser.parse_args()
-read_data_from = os.path.realpath(args.file_input)
-write_valid_data_to = os.path.realpath(args.file_output)
-validate_flag = args.validate
-sort_flag = args.sort
-count = args.count
+parser.add_argument(
+    '-n',
+    '-process_number',
+    type=int,
+    help='Количество процессов',
+    required=True,
+    dest='process_number'
+)
 
-if sort_flag:
-    try:
-        data = read_valid_txt(read_data_from)
-        if count:
-            data = data[0:count]
-        data = insertion_sort(data, height_value)
-        serialize_validators_to_pickle(data, write_valid_data_to)
-        data = deserialize_validators_from_pickle(write_valid_data_to)
-        if write_valid_data_to.endswith('.txt'):
-            write_valid_data_to = write_valid_data_to.removesuffix('.txt')
-            write_valid_data_to = write_valid_data_to + '.json'
-        serialize_validators_to_json(data_list=data, file=write_valid_data_to)
-    except FileNotFoundError:
-        print('Файл не найден, проверьте пути к файлам')
-    except BaseException as ex:
-        print(ex)
-        print('Неизвестная ошибка, повторите попытку, перед этим проверив пути к файлам и формат записей в них')
-    else:
-        print("Программа успешно завершена")
-elif validate_flag:
-    try:
-        file = File(read_data_from)
-        with tqdm(file.data, desc='Заполняем словари мировоззрений, политических взглядов, профессий') as progressbar:
-            with open(write_valid_data_to, mode='w', encoding='windows-1251') as write_to_file:
-                for record in file.data:
-                    validate = Validator(
-                        str(
-                            record['email']), str(
-                            record['height']), str(
-                            record['snils']), str(
-                            record['passport_number']), str(
-                            record['occupation']), str(
-                                record['age']), str(
-                                    record['political_views']), str(
-                                        record['worldview']), str(
-                                            record['address']))
-                    validate.fill_political_views_dict()
-                    validate.fill_world_view_dict()
-                    validate.fill_occupation_view_dict()
-                    progressbar.update(1)
-        data_to_save = []
-        with tqdm(file.data, desc='Проверяем записи на соответствие критериям и записываем их в файлы') as progressbar:
-            with open(write_valid_data_to, mode='wb') as write_to_file:
-                for record in file.data:
-                    validate = Validator(
-                        str(
-                            record['email']), str(
-                            record['height']), str(
-                            record['snils']), str(
-                            record['passport_number']), str(
-                            record['occupation']), str(
-                                record['age']), str(
-                                    record['political_views']), str(
-                                        record['worldview']), str(
-                                            record['address']))
-                    if validate.statistic_is_record_correct():
-                        record['height'] = float(record['height'])
-                        data_to_save.append(record)
-                    progressbar.update(1)
-                pickle.dump(data_to_save, write_to_file)
-            if write_valid_data_to.endswith('.txt'):
-                write_valid_data_to = write_valid_data_to.removesuffix('.txt')
-                write_valid_data_to = write_valid_data_to + '.json'
-            with open(write_valid_data_to, mode='w') as write_to_file:
-                json.dump(data_to_save, write_to_file, ensure_ascii=False, indent=1)
-        Validator.print_statistics()
-    except BaseException as ex:
-        print(ex)
-        print("Произошла ошибка, проверьте пути к файлам")
-else:
-    raise SystemExit(1)
+
+def square(x: int or float) -> int or float:
+    return x*x
+
+
+def to_the_power(x: int or float, n: int) -> int or float:
+    return x**n
+
+
+def expected_math_value(list_of_numbers: list, list_of_possibilities: list, power_of_moment: int) -> float:
+    mx = 0
+    if len(list_of_numbers) != len(list_of_possibilities):
+        raise ValueError('The length of lists are not equal')
+    list_of_numbers = list(map(lambda x: x**power_of_moment, list_of_numbers))
+
+    for i in range(len(list_of_numbers)):
+        mx += list_of_numbers[i]*list_of_possibilities[i]
+    return mx
+
+
+def dispersion(list_of_numbers: list, list_of_possibilities: list) -> float:
+    m2x = expected_math_value(list_of_numbers, list_of_possibilities, 2)
+    mx2 = expected_math_value(list_of_numbers, list_of_possibilities, 1)**2
+    return m2x - mx2
+
+
+def expected_mx(list_of_numbers: list) -> float:
+    sum_of_numbers = sum(list_of_numbers)
+    return sum_of_numbers/len(list_of_numbers)
+
+
+def minus_expected(x):
+    return (x - expected_mx_) ** 2
+
+
+args = parser.parse_args()
+first_number = args.first_number
+second_number = args.second_number
+third_number = args.third_number
+process_number = args.process_number
+numbers = [first_number, second_number, third_number]
+expected_mx_ = expected_mx(numbers)
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    n = process_number
+    print(f'Количество процессов:{n}')
+    with Pool(n) as p:
+        # print(p.map(lambda x: (x - expected_mx_)**2, [first_number, second_number, third_number]))
+        res = p.map(minus_expected, numbers)
+    result = sum(res)/len(res)
+
+    print(f"Введенные числа: {first_number} , {second_number}, {third_number}")
+    print(f'Дисперсия введенных чисел: {result}')
+    print("--- Программа выполнена за: %s ---" % (time.time() - start_time))
